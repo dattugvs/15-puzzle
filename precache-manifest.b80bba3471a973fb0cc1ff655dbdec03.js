00974f05ee655d357902287f57c680aeef6644b8
datugvs <dattugvs@gmail.com>
2019-11-15 16:30:11 +0530

    Updates

---
+++ precache-manifest.b80bba3471a973fb0cc1ff655dbdec03.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c780f0ae6ff4657d4a4dd5419620d2ec",
+    "url": "/15-puzzle/index.html"
+  },
+  {
+    "revision": "cf59f29edea0c78923da",
+    "url": "/15-puzzle/static/js/2.00b210ae.chunk.js"
+  },
+  {
+    "revision": "0786646cd6ce9ede710a",
+    "url": "/15-puzzle/static/js/main.43a5dbc2.chunk.js"
+  },
+  {
+    "revision": "2a6eb34c1e68a36964fd",
+    "url": "/15-puzzle/static/js/runtime~main.6b8d6fb7.js"
+  }
+]);
